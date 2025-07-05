b1c7979ec87b7560facaf5233ffdeea84af39b24
Kotaro <ino.kota0602@gmail.com>
2025-07-05 20:15:11 +0900

    docs: mainセクション書換え

--- src/components/Lines.jsx
+++ src/components/Lines.jsx
@@ -24,6 +24,7 @@ export default function Lines({ lines, siteColor }) {
                 className="object-cover"
               />
             </div>
+
             <div className="p-5 pt-2 pb-2 text-center text-2xl font-semibold text-gray-700">
               {name}
             </div>
